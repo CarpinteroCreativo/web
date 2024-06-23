4cb7fdb64ef116b26bae62eefbf5c7cb29b55987
CarpinteroCreativo <61610433+CarpinteroCreativo@users.noreply.github.com>
2024-06-23 14:29:12 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//cliente
+let cliente = ['Edwin Ramirez'];
+document.querySelector("#cliente").innerHTML = cliente[0];
+
+//Saldo inicial.
+let deudaInicial = 20000;
+document.querySelector("#sInicial").innerHTML = deudaInicial;
+
+//Abonos.
+let abonado = 0;
+document.querySelector("#abonado").innerHTML = abonado;
+
+//Saldo Actual
+let saldoActual = deudaInicial - abonado;
+document.querySelector("#saActual").innerHTML = saldoActual;
+
+
+
+
+
+
