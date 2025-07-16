60ed1bcdccadd622a7cefeba3eb850bd521a3122
CarpinteroCreativo <61610433+CarpinteroCreativo@users.noreply.github.com>
2025-07-16 11:00:03 -0600

    Actualizar app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let deudaInicial = 20000;
 document.querySelector("#sInicial").innerHTML = deudaInicial;
 
 //Abonos.
-let abonado = 11000;
+let abonado = 12000;
 document.querySelector("#abonado").innerHTML = abonado;
 
 //Saldo Actual
